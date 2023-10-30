b6c9b22f57a8f2c96977a6a3b39460eb9a2f49b4
unknown <alexkinkriashvili@gmail.com>
2023-10-30 17:07:03 +0400

    Adding travel list app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function Logo() {
+  return <h1>Far Away</h1>;
+}
+function Form({ setItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { id: Date.now(), description, quantity, packed: false };
+    setItems((prev) => [...prev, newItem]);
+    setDescription("");
+    setQuantity(1);
+  }
+
+  return (
+    <form name="form" className="add-form" onSubmit={handleSubmit}>
+      <h2>What do you need for your trip</h2>
+      <select
+        name="select"
+        onChange={(e) => setQuantity(Number(e.target.value))}
+        value={quantity}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+        ;
+      </select>
+      <input
+        name="input"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+        type="text"
+        placeholder="item..."
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList({ setItems, items, handleDeleteItem, handleToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            handleToggleItem={handleToggleItem}
+            handleDeleteItem={handleDeleteItem}
+            item={item}
+            key={item.id}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+        <button onClick={() => setItems([])}> Clear List</button>
+      </div>
+    </div>
+  );
+}
+function Item({ item, handleDeleteItem, handleToggleItem }) {
+  return (
+    <li key={item.id}>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => handleToggleItem(item.id)}
+      />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button onClick={() => handleDeleteItem(item.id)}> &times;</button>
+    </li>
+  );
+}
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go"
+          : `You have ${numItems} items on your list, and you already packed{" "}
+        ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form setItems={setItems} />
+      <PackingList
+        setItems={setItems}
+        handleToggleItem={handleToggleItem}
+        items={items}
+        handleDeleteItem={handleDeleteItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
